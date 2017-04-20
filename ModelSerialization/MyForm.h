f941441a7eabe1286e64131e5284d338711c99fa
romas <romashkin3355@gmail.com>
2017-04-20 22:39:43 +0300

    add pivot control

--- ModelSerialization/MyForm.h
+++ ModelSerialization/MyForm.h
@@ -42,6 +42,8 @@ namespace ModelSerialization {
 	private: System::Windows::Forms::PictureBox^  pictureBox1;
 	private: System::Windows::Forms::Button^  button2;
 	private: System::Windows::Forms::TextBox^  textBox1;
+	private: System::Windows::Forms::TextBox^  textBox2;
+	private: System::Windows::Forms::Label^  label1;
 	protected:
 
 	private:
@@ -61,12 +63,14 @@ namespace ModelSerialization {
 			this->pictureBox1 = (gcnew System::Windows::Forms::PictureBox());
 			this->button2 = (gcnew System::Windows::Forms::Button());
 			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
+			this->textBox2 = (gcnew System::Windows::Forms::TextBox());
+			this->label1 = (gcnew System::Windows::Forms::Label());
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->BeginInit();
 			this->SuspendLayout();
 			// 
 			// button1
 			// 
-			this->button1->Location = System::Drawing::Point(277, 400);
+			this->button1->Location = System::Drawing::Point(200, 400);
 			this->button1->Name = L"button1";
 			this->button1->Size = System::Drawing::Size(103, 63);
 			this->button1->TabIndex = 0;
@@ -84,7 +88,7 @@ namespace ModelSerialization {
 			// 
 			// button2
 			// 
-			this->button2->Location = System::Drawing::Point(449, 400);
+			this->button2->Location = System::Drawing::Point(475, 400);
 			this->button2->Name = L"button2";
 			this->button2->Size = System::Drawing::Size(75, 63);
 			this->button2->TabIndex = 2;
@@ -94,17 +98,36 @@ namespace ModelSerialization {
 			// 
 			// textBox1
 			// 
-			this->textBox1->Location = System::Drawing::Point(553, 418);
+			this->textBox1->Location = System::Drawing::Point(576, 418);
 			this->textBox1->Name = L"textBox1";
 			this->textBox1->Size = System::Drawing::Size(113, 26);
 			this->textBox1->TabIndex = 3;
 			this->textBox1->Text = Convert::ToString(15);
+			// 
+			// textBox2
+			// 
+			this->textBox2->Location = System::Drawing::Point(341, 437);
+			this->textBox2->Name = L"textBox2";
+			this->textBox2->Size = System::Drawing::Size(100, 26);
+			this->textBox2->TabIndex = 4;
+			this->textBox2->Text = Convert::ToString(100);
+			// 
+			// label1
+			// 
+			this->label1->AutoSize = true;
+			this->label1->Location = System::Drawing::Point(364, 414);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(51, 20);
+			this->label1->TabIndex = 5;
+			this->label1->Text = L"Pivot";
 			// 
 			// MyForm
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(9, 20);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
 			this->ClientSize = System::Drawing::Size(778, 544);
+			this->Controls->Add(this->label1);
+			this->Controls->Add(this->textBox2);
 			this->Controls->Add(this->textBox1);
 			this->Controls->Add(this->button2);
 			this->Controls->Add(this->pictureBox1);
@@ -164,7 +187,7 @@ namespace ModelSerialization {
 			matrix->Add(-Math::Sin(radians));
 			matrix->Add(Math::Sin(radians));
 			matrix->Add(Math::Cos(radians));
-			int d = 100;
+			int d = Convert::ToInt16(textBox2->Text);
 			for each(PointF point in points) {
 				double x = (double)point.X - d;
 				double y = (double)point.Y - d;
